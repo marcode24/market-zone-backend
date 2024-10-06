f986fa43b23c636e84e5e35934f5bd8d6b81c244
Marco Cruz <marco24cruz08@gmail.com>
2024-10-05 23:56:34 -0600

    ♻️ Refactor Error class in Domain.Abstractions

--- src/Domain/Abstractions/Error.cs
+++ src/Domain/Abstractions/Error.cs
@@ -1,19 +1,7 @@
 namespace Domain.Abstractions;
 
-/// <summary>
-/// Represents an error in the domain.
-/// </summary>
-/// <param name="Code">The error code.</param>
-/// <param name="Message">The error message.</param>
 public record Error(string Code, string Message)
 {
-  /// <summary>
-  /// Represents an error that indicates that the value is null.
-  /// </summary>
   public static readonly Error None = new(string.Empty, string.Empty);
-
-  /// <summary>
-  /// Represents an error that indicates that the value is null.
-  /// </summary>
   public static readonly Error NullValue = new("NULL_VALUE", "The value should not be null.");
 }
