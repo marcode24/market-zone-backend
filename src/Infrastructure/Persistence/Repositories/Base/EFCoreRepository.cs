113f560ba9d70f6e0238fb8c58f3e2b67f7dfdf5
Marco Cruz <marco24cruz08@gmail.com>
2024-10-18 23:24:37 -0600

    ✨ Refactor EFCoreRepository to include GetByIdIncludingDeletedAsync method

---
+++ src/Infrastructure/Persistence/Repositories/Base/EFCoreRepository.cs
@@ -0,0 +1,40 @@
+using System.Data;
+using System.Text;
+using Application.Abstractions.Data;
+using Dapper;
+using Domain.Abstractions;
+using Domain.Repositories.Paginations;
+using Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories.Base;
+
+internal abstract class EFCoreRepository<TEntity, TEntityId>
+  where TEntity : Entity<TEntityId>
+  where TEntityId : class
+{
+  protected readonly ApplicationDbContext _dbContext;
+
+  public EFCoreRepository(ApplicationDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
+  public async Task<TEntity?> GetByIdAsync(TEntityId id, CancellationToken cancellationToken)
+  {
+    return await _dbContext.Set<TEntity>()
+      .SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+  }
+
+  public async Task<TEntity?> GetByIdIncludingDeletedAsync(TEntityId id, CancellationToken cancellationToken)
+  {
+    return await _dbContext.Set<TEntity>()
+      .IgnoreQueryFilters()
+      .SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+  }
+
+  public virtual void Add(TEntity entity)
+  {
+    _dbContext.Set<TEntity>().Add(entity);
+  }
+}
